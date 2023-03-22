a6d17a108f3bdbfe7952df8766879ece39ee997e
Lior Shiboli <lior12sh@gmail.com>
2023-03-22 12:03:53 +0200

    first draft

---
+++ AdptArray.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include "AdptArray.h"
+typedef struct AdptArray_
+{
+    PElement * ElemArr;
+    int size;
+    DEL_FUNC Delete;
+    COPY_FUNC Copy;
+    PRINT_FUNC Print;
+}AdptArray; 
+PAdptArray CreateAdptArray(COPY_FUNC CopyFunc, DEL_FUNC DeleteFunc,PRINT_FUNC PrintFunc){
+    PAdptArray  Arr = (PAdptArray) malloc(sizeof(AdptArray));
+    if (!Arr)
+    {
+        return NULL;
+    }
+    
+    Arr->Copy = CopyFunc;
+    Arr->Delete = DeleteFunc;
+    Arr-> Print = PrintFunc;
+    Arr->size = 0;
+    Arr ->ElemArr =NULL;
+    return Arr;
+}
+void DeleteAdptArray(PAdptArray Arr)
+{
+    for (size_t i = 0; i < Arr->size; i++)
+    {
+        if (Arr->ElemArr[i])
+        {
+            Arr->Delete(Arr->ElemArr[i]);
+        }
+        
+    }
+    free(Arr);
+}
+void PrintDB(PAdptArray Arr){
+     for (size_t i = 0; i < Arr->size; i++)
+    {
+        if (Arr->ElemArr[i])
+        {
+            Arr->Print(Arr->ElemArr[i]);
+        }
+        
+    }
+}
+int GetAdptArraySize(PAdptArray Arr){
+    if (Arr==NULL)
+    {
+        return -1;
+    }
+    return Arr->size;
+}
+
+PElement GetAdptArrayAt(PAdptArray Arr, int Place){
+    if (Arr == NULL|| Arr ->size <= Place)
+    {
+        return NULL;
+    }
+    return Arr->ElemArr[Place];
+
+}
+Result SetAdptArrayAt(PAdptArray Arr, int Place, PElement Element){
+if (!Arr)
+{
+    return FAIL;
+}
+if (Arr->size<=Place){
+    PElement *NewElemArr = (PElement*) calloc(sizeof(PElement),(Place+1));
+    if (NewElemArr==NULL)
+    {
+        return FAIL;
+    }
+    
+    for (int i=0; i < Arr->size; i++)
+    {
+        NewElemArr[i]= Arr->ElemArr[i];
+    }
+    free(Arr->ElemArr);
+    Arr->ElemArr= NewElemArr;
+    Arr->size=Place +1;
+}
+if (Arr->ElemArr[Place]!=NULL)
+{
+    Arr->Delete(Arr->ElemArr[Place]);
+}
+Arr->ElemArr[Place] = Arr->Copy(Element);
+return SUCCESS;
+}
